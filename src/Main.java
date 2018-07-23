8c37584a414f4d9dd04361d2650e7056d3e4fd14
Ryan Bachman <jacksprat455@gmail.com>
2018-07-23 15:13:46 -0500

    Methods_Exercise

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        numberCounter();
+//
+//    }
+//
+//    public static void numberCounter() {
+//        int counter = 0;
+//        while (counter < 6) {
+//            System.out.println(counter);
+//            counter += 1;
+//        }
+//    }
+//}
+//
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//                numberCounter();
+//                otherNumberCounter();
+//                anotherNumberCounter();
+//    }
+//
+//    public static void numberCounter() {
+//        int counter = 0;
+//        while (counter < 6) {
+//            System.out.println(counter);
+//            counter += 1;
+//        }
+//    }
+//
+//        public static void otherNumberCounter () {
+//
+//        int counter = 0;
+//
+//        while (counter < 6) {
+//            System.out.println(counter);
+//            counter += 1;
+//        }
+//    }
+//
+//
+//
+//    public static void anotherNumberCounter () {
+//        int counter = 0;
+//        while (counter < 6) {
+//            System.out.println(counter);
+//            counter += 1;
+//        }
+//    }
+//}
+
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+
+    }
+    public static void number(){
+        System.out.println("");
+
+
+    }
+}
